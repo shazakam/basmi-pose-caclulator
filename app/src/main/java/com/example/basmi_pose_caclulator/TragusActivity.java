e5f52ce210ae8e8dd8b9bb1e7da796469a792de0
shazakam <76884408+shazakam@users.noreply.github.com>
2022-06-12 20:23:28 +0100

    Image storage-ish

--- app/src/main/java/com/example/basmi_pose_caclulator/TragusActivity.java
+++ app/src/main/java/com/example/basmi_pose_caclulator/TragusActivity.java
@@ -8,38 +8,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.hbisoft.pickit.PickiT;
+import com.hbisoft.pickit.PickiTCallbacks;
+
 import java.net.URI;
+import java.util.ArrayList;
 
 public class TragusActivity extends AppCompatActivity {
 
     //URI of images needs to be stored from OnClickTragusImage
     //Note to self may not be URI but BitMap instead
 
-    URI tragusImageOne;
-    URI tragusImageTwo;
 
-    //This tells what getImage should do with the result from the intent
-    ActivityResultLauncher<Intent> getImage = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
-            new ActivityResultCallback<ActivityResult>() {
-                @Override
-                //What should be done once the result from the intent has been received
-                public void onActivityResult(ActivityResult result) {
-                    if (result.getResultCode() == RESULT_OK && result.getData() != null) {
-                        Context context = getApplicationContext();
-                        CharSequence text = "Upload Succesful!";
-                        int duration = Toast.LENGTH_SHORT;
+    Uri tragusImageOne;
+    Uri tragusImageTwo;
+    ImageView imageView;
 
-                        Toast toast = Toast.makeText(context, text, duration);
-                        toast.show();
-                    }
-                }
-            }
-    );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,8 +40,35 @@ public class TragusActivity extends AppCompatActivity {
         setContentView(R.layout.activity_tragus);
     }
 
-    public void onClickTragusImage(View view){
-        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+    //This tells what getImage should do with the result from the intent
+    ActivityResultLauncher<Intent> getImage = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                //What should be done once the result from the intent has been received
+                public void onActivityResult(ActivityResult result) {
+
+                    if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+                        Bundle extras = result.getData().getExtras();
+                        Bitmap imageBitmap = (Bitmap) extras.get("data");
+
+
+                        //Toast
+                        Context context = getApplicationContext();
+                        CharSequence text = "Upload Succesful!";
+                        int duration = Toast.LENGTH_SHORT;
+                        Toast toast = Toast.makeText(context, text, duration);
+                        toast.show();
+                    }
+                }
+            }
+    );
+
+
+    public void onClickTragusImage(View view) {
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         getImage.launch(intent);
+
     }
+
 }
