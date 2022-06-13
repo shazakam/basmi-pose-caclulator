7485143b8e3207c37cb083dac5bb7c6c03e68ff8
shazakam <76884408+shazakam@users.noreply.github.com>
2022-06-13 16:45:18 +0100

    Camera implemented alongside Google ML Kit Pose Detection Calculated Poses are stored for TragusActivity in two seperate variables namely, poseOne and poseTwo. Landmarks for each can be printed into Log with Button on the app.

--- app/src/main/java/com/example/basmi_pose_caclulator/TragusActivity.java
+++ app/src/main/java/com/example/basmi_pose_caclulator/TragusActivity.java
@@ -1,43 +1,58 @@
 package com.example.basmi_pose_caclulator;
-
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.pose.Pose;
+import com.google.mlkit.vision.pose.PoseDetection;
+import com.google.mlkit.vision.pose.PoseDetector;
+import com.google.mlkit.vision.pose.PoseLandmark;
+import com.google.mlkit.vision.pose.accurate.AccuratePoseDetectorOptions;
 
-import com.hbisoft.pickit.PickiT;
-import com.hbisoft.pickit.PickiTCallbacks;
+import java.util.List;
 
-import java.net.URI;
-import java.util.ArrayList;
 
 public class TragusActivity extends AppCompatActivity {
 
     //URI of images needs to be stored from OnClickTragusImage
     //Note to self may not be URI but BitMap instead
+    //MVP = Minimun Viable Product
+    Pose poseOne = null;
+    Pose poseTwo = null;
 
+    PoseDetector tragusPoseDetector;
 
-    Uri tragusImageOne;
-    Uri tragusImageTwo;
-    ImageView imageView;
-
+    AccuratePoseDetectorOptions options =
+            new AccuratePoseDetectorOptions.Builder()
+                    .setDetectorMode(AccuratePoseDetectorOptions.SINGLE_IMAGE_MODE)
+                    .build();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tragus);
+
+        //Initialise pose detector with defined options above
+        tragusPoseDetector = PoseDetection.getClient(options);
+
     }
 
     //This tells what getImage should do with the result from the intent
@@ -50,6 +65,35 @@ public class TragusActivity extends AppCompatActivity {
                     if (result.getResultCode() == RESULT_OK && result.getData() != null) {
                         Bundle extras = result.getData().getExtras();
                         Bitmap imageBitmap = (Bitmap) extras.get("data");
+
+                        InputImage inputImage = InputImage.fromBitmap(imageBitmap, 90);
+
+                        Task<Pose> poseResult =
+                                tragusPoseDetector.process(inputImage)
+                                        .addOnSuccessListener(
+                                                new OnSuccessListener<Pose>() {
+                                                    @Override
+                                                    public void onSuccess(Pose pose) {
+
+                                                        if(poseOne == null){
+                                                            poseOne = pose;
+
+                                                        }
+                                                        else if(poseTwo == null){
+                                                            poseTwo = pose;
+                                                        }
+
+                                                        // Task completed successfully
+                                                        PoseLandmark leftShoulder = pose.getPoseLandmark(PoseLandmark.LEFT_SHOULDER);
+                                                    }
+                                                })
+                                        .addOnFailureListener(
+                                                new OnFailureListener() {
+                                                    @Override
+                                                    public void onFailure(@NonNull Exception e) {
+
+                                                    }
+                                                });
 
 
                         //Toast
@@ -64,11 +108,21 @@ public class TragusActivity extends AppCompatActivity {
     );
 
 
-    public void onClickTragusImage(View view) {
-
+    public void onClickTragusImageBtnOne(View view) {
+        //Intent to take Photo
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         getImage.launch(intent);
 
+    }
+
+    public void btnPrintPose(View view){
+        for(PoseLandmark p: poseOne.getAllPoseLandmarks()){
+            Log.d("Pose One LANDMARK TYPE " +  String.valueOf(p.getLandmarkType()), p.getPosition().toString());
+        }
+
+        for(PoseLandmark p: poseTwo.getAllPoseLandmarks()){
+            Log.d("Pose Two LANDMARK TYPE " +  String.valueOf(p.getLandmarkType()), p.getPosition().toString());
+        }
     }
 
 }
